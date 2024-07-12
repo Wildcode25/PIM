6fb2a03fba9a0ec666ee3ab8a4516e8e16b6e44e
Wildcode25 <enmanuelcastillo1999@gmail.com>
2024-07-12 17:46:46 -0400

    redesing db

--- config/PIM.sql
+++ config/PIM.sql
@@ -1,3 +1,6 @@
+DROP DATABASE PIM IF EXISTS;
+CREATE DATABASE PIM;
+USE DATABASE PIM;
 CREATE TABLE customer (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50),
@@ -9,13 +12,28 @@ CREATE TABLE product (
     name VARCHAR(100) NOT NULL, 
     description TEXT, 
     price DECIMAL(10, 2) NOT NULL,
-    stock INT NOT NULL, 
+    stock INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	customer_id int,
 	FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
-alter table product add column categories varchar(10);
-
+CREATE TABLE category (
+    id SERIAL PRIMARY KEY,
+    name varchar(50),
+);
+CREATE TABLE product_category (
+    product_id PRIMARY KEY,
+    category_id PRIMARY KEY,
+    FOREIGN KEY(product_id) REFERENCES product(id);
+    FOREIGN KEY (category_id) REFERENCES category(id);
+);
+INSERT INTO category(name) VALUES
+("kitchen"),
+("appliances"),
+("cleaning"),
+("laundry"),
+("cooling"),
+("home");
 INSERT INTO customer(name, email, password)
 VALUES('Emmanuel', 'enmanuelcastillo1999@gmail.com', 'mordex123');
